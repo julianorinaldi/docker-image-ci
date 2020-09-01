93abd5b266e570956e5f6ea4d63b9ecd6e067b6e
Juliano Tiago Rinaldi <julianorinaldi@gmail.com>
2020-08-31 21:06:47 -0300

    Change time increment and amount counter

--- docker-git-hello-world/src/main/java/org/pictolearn/docker/HelloWorldPing.java
+++ docker-git-hello-world/src/main/java/org/pictolearn/docker/HelloWorldPing.java
@@ -3,9 +3,9 @@ package org.pictolearn.docker;
 public class HelloWorldPing {
 	
 	public static void main(String args[]) throws Exception{
-		for(int i = 0 ; i < 100; i++){
+		for(int i = 0 ; i < 1000; i++){
 			System.out.println("Hello World Ping " + i );
-			Thread.sleep(1000);
+			Thread.sleep(100);
 		}
 	}
 }
